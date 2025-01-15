c8eee217ca7ddc90339993956b0cd08e04a26269
Cauzzmo <elijahmarks751@gmail.com>
2025-01-15 12:47:38 -0500

    init

---
+++ src/main/java/org/cosmo/elijah/cosmoBasics.java
@@ -0,0 +1,17 @@
+package org.cosmo.elijah;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class cosmoBasics extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
